d0a15cc48c7936d4f733b722005ab2f11abf677a
Arya Priema <99aryaprima@gmail.com>
2024-02-04 00:55:42 +0700

    setup font, colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen flex-col">
       <Button>Button</Button>
+      <p className="text-silver dm_sans__body">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
     </div>
   );
 }
